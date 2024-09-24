8d59b7b113e74441343218ad603e36175b0f1203
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2024-09-24 14:10:27 +0000

    add next ao gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Welcome to Next.js!</h1>
+  return (
+  <div>
+    <h1>NextJS acordado :D</h1>
+    <h2>Página home de alguma coisa.</h2>
+    <p>codespaces público</p>
+  </div>
+  
+)
+  
+
 }
 
 export default Home;
